e9995ad808c561558051d481ec5389c8de277258
Julian Rüth <julian.rueth@fsfe.org>
2019-10-02 00:50:30 +0200

    do not force std=c++14 for macOS

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,6 +4,10 @@ if [ "$(uname)" == "Darwin" ]; then
     export CXXFLAGS="$CXXFLAGS -fno-common"
 fi
 
+# Drop any C++ standard set by the compiler. ./configure finds the correct
+# flags for us:
+export CXXFLAGS=`echo $CXXFLAGS | sed 's/ -std=[^ ]*/ /g'`
+
 ./bootstrap.sh
 ./configure --prefix="$PREFIX" || (cat config.log && false)
 
